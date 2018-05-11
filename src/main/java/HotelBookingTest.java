965c32d8127c98a22ef86100e0b2326ca45b5d66
thakurs <thakurs@MIT-9119-L.mscpla.local>
2018-05-11 11:42:06 +0530

    modifying the code

--- src/main/java/HotelBookingTest.java
+++ src/main/java/HotelBookingTest.java
@@ -1,4 +1,8 @@
 import com.sun.javafx.PlatformUtil;
+
+import utilities.ApplicationUtilities;
+import utilities.BaseDriver;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -6,48 +10,41 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
-public class HotelBookingTest {
+public class HotelBookingTest extends BaseDriver {
 
-    WebDriver driver = new ChromeDriver();
+	ApplicationUtilities utilties = new ApplicationUtilities(driver) ;
 
-    @FindBy(linkText = "Hotels")
-    private WebElement hotelLink;
+	@FindBy(linkText = "Hotels")
+	private WebElement hotelLink;
 
-    @FindBy(id = "Tags")
-    private WebElement localityTextBox;
+	@FindBy(id = "Tags")
+	private WebElement localityTextBox;
 
-    @FindBy(id = "SearchHotelsButton")
-    private WebElement searchButton;
+	@FindBy(id = "SearchHotelsButton")
+	private WebElement searchButton;
 
-    @FindBy(id = "travellersOnhome")
-    private WebElement travellerSelection;
+	@FindBy(id = "travellersOnhome")
+	private WebElement travellerSelection;
 
-    @Test
-    public void shouldBeAbleToSearchForHotels() {
-        setDriverPath();
+	@Test
+	public void shouldBeAbleToSearchForHotels() {
 
-        driver.get("https://www.cleartrip.com/");
-        hotelLink.click();
+		driver.get("https://www.cleartrip.com/");
+		utilties.clickElementBy(hotelLink);
 
-        localityTextBox.sendKeys("Indiranagar, Bangalore");
+		localityTextBox.sendKeys("Indiranagar, Bangalore");
 
-        new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
-        searchButton.click();
+		if(travellerSelection.isDisplayed()){
+			Select sel = new Select(travellerSelection);
+			sel.selectByVisibleText("1 room, 2 adults");
+		}
+		else
 
-        driver.quit();
+		System.out.println("dropdown is not displayed");
+		utilties.clickElementBy(searchButton);
 
-    }
 
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }
+	}
+
 
 }
